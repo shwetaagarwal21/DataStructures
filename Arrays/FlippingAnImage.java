7cea851865910e18626433c01e8e789033274c8d
shwetaagarwal21 <shweta.agarwal21@gmail.com>
2018-05-21 16:41:35 -0700

    Create FlippingAnImage.java

---
+++ Arrays/FlippingAnImage.java
@@ -0,0 +1,23 @@
+class FlippingAnImage {
+  public int[][] flipAndInvertImage(int[][] A) {
+        int k = 0;
+        int[][] B = new int[A.length][A[0].length];
+        for(int row = 0; row < A.length; row++){
+            k = 0;
+            for(int col=A[row].length-1; col >= 0; col--){
+                B[row][col] = A[row][k];
+                k++;
+            }
+        }
+        
+        for(int row = 0; row < B.length; row++){
+            for(int col=0; col < B[row].length; col++){
+                if(B[row][col] == 0)
+                    B[row][col] = 1;
+                else
+                    B[row][col] = 0;
+            }
+        }
+        return B;
+    }
+}
